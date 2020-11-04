9be80cd9448be48c517ef7259b5a57c22698864c
daichi.murakami <daichi.murakami@tier4.jp>
2020-11-04 21:30:05 +0900

    launch file with blob_size arguments

--- src/comm/launch/inter_process_pubsub.launch.py
+++ src/comm/launch/inter_process_pubsub.launch.py
@@ -1,25 +1,29 @@
 import launch
 import launch_ros.actions
 
-BLOB_SIZE = 2000;
-
 def generate_launch_description():
+    blob_size = launch.substitutions.LaunchConfiguration('blob_size')
     publisher = launch_ros.actions.Node(
         package='comm',
         node_executable='publisher',
         output='screen',
-        parameters=[{'blob_size':BLOB_SIZE}]
+        parameters=[{'blob_size':blob_size}]
     )
     subscriber = launch_ros.actions.Node(
         package='comm',
         node_executable='subscriber',
         output='screen',
-        parameters=[{'blob_size':BLOB_SIZE}]
+        parameters=[{'blob_size':blob_size}]
     )
 
     return launch.LaunchDescription([
         publisher,
         subscriber,
+        launch.actions.DeclareLaunchArgument(
+            'blob_size',
+            default_value=['1000'],
+            description='blob data size; default 1000 [Byte]'
+        ),
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=publisher,
--- src/comm/launch/intra_process_pubsub.launch.py
+++ src/comm/launch/intra_process_pubsub.launch.py
@@ -1,18 +1,22 @@
 import launch
 import launch_ros.actions
 
-BLOB_SIZE = 2000;
-
 def generate_launch_description():
+    blob_size = launch.substitutions.LaunchConfiguration('blob_size')
     pubsub = launch_ros.actions.Node(
         package='comm',
         node_executable='pubsub',
         output='screen',
-        parameters=[{'blob_size':BLOB_SIZE}]
+        parameters=[{'blob_size':blob_size}]
     )
 
     return launch.LaunchDescription([
         pubsub,
+        launch.actions.DeclareLaunchArgument(
+            'blob_size',
+            default_value=['1000'],
+            description='blob data size; default 1000 [Byte]'
+        ),
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=pubsub,
