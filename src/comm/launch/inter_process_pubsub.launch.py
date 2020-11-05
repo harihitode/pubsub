3b7da86d77d82b8cf56cf69fd49b295597c76e88
daichi.murakami <daichi.murakami@tier4.jp>
2020-11-05 16:02:13 +0900

    enable default value

--- src/comm/launch/inter_process_pubsub.launch.py
+++ src/comm/launch/inter_process_pubsub.launch.py
@@ -2,31 +2,33 @@ import launch
 import launch_ros.actions
 
 def generate_launch_description():
-    blob_size = launch.substitutions.LaunchConfiguration('blob_size')
+    arguments = launch.actions.DeclareLaunchArgument(
+        'blob_size',
+        default_value=['1000'],
+        description='blob data size; default 1000 [Byte]'
+    )
+
     publisher = launch_ros.actions.Node(
         package='comm',
         node_executable='publisher',
         output='screen',
-        parameters=[{'blob_size':blob_size}]
+        parameters=[{'blob_size':launch.substitutions.LaunchConfiguration('blob_size')}]
     )
     subscriber = launch_ros.actions.Node(
         package='comm',
         node_executable='subscriber',
-        output='screen',
-        parameters=[{'blob_size':blob_size}]
+        output='log',
+        parameters=[{'blob_size':launch.substitutions.LaunchConfiguration('blob_size')}]
     )
 
     return launch.LaunchDescription([
+        arguments,
         publisher,
         subscriber,
-        launch.actions.DeclareLaunchArgument(
-            'blob_size',
-            default_value=['1000'],
-            description='blob data size; default 1000 [Byte]'
-        ),
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=publisher,
                 on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-            )),
+            )
+        ),
     ])
--- src/comm/launch/intra_process_pubsub.launch.py
+++ src/comm/launch/intra_process_pubsub.launch.py
@@ -2,24 +2,26 @@ import launch
 import launch_ros.actions
 
 def generate_launch_description():
-    blob_size = launch.substitutions.LaunchConfiguration('blob_size')
+    arguments = launch.actions.DeclareLaunchArgument(
+        'blob_size',
+        default_value=['1000'],
+        description='blob data size; default 1000 [Byte]'
+    )
+
     pubsub = launch_ros.actions.Node(
         package='comm',
         node_executable='pubsub',
-        output='screen',
-        parameters=[{'blob_size':blob_size}]
+        output='log',
+        parameters=[{'blob_size':launch.substitutions.LaunchConfiguration('blob_size')}]
     )
 
     return launch.LaunchDescription([
+        arguments,
         pubsub,
-        launch.actions.DeclareLaunchArgument(
-            'blob_size',
-            default_value=['1000'],
-            description='blob data size; default 1000 [Byte]'
-        ),
         launch.actions.RegisterEventHandler(
             event_handler=launch.event_handlers.OnProcessExit(
                 target_action=pubsub,
                 on_exit=[launch.actions.EmitEvent(event=launch.events.Shutdown())],
-            )),
+            )
+        ),
     ])
